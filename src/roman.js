d5b63e49633eb6c6f6df229bc9092c0a7bb88e81
unknown <setom20@gmail.com>
2020-10-29 18:06:24 +0100

    toString function

--- src/roman.js
+++ src/roman.js
@@ -1,9 +1,18 @@
+
+const arabicNumerals = { M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1 };
+
 function RomanNumber(value) {
   this.value = value;
 }
 
 RomanNumber.prototype.toString = function () {
-  return this.value;
+  let tmpNum = this.value;
+  let result = "";
+  Object.keys(arabicNumerals).forEach( (key) => {
+    result += key.repeat((tmpNum / arabicNumerals[key]) >>> 0); // equivalent of Math.floor but for null returns 0
+    tmpNum %= arabicNumerals[key];
+  });
+  return result;
 };
 
 RomanNumber.prototype.toInt = function () {
